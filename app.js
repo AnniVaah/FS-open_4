92805eb33f33a64d6742f4f75093a4a44c2f4fa1
Anni <anni.o.vaahtoranta@gmail.com>
2024-04-15 15:08:40 +0300

    routejen tapahtumankäsittelijät tiedostoon controllers/blogs.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 
@@ -15,22 +18,6 @@ app.use(cors())
 // app.use(express.static('dist'))
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
