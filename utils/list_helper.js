a1a99870eb4615a4468580ee1605d9504ffee5fc
Anni <anni.o.vaahtoranta@gmail.com>
2024-04-16 11:06:53 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,6 @@
 const dummy = (blogs) => 1
 
-module.exports ={ dummy }
+const totalLikes = (blogs) =>
+  blogs.reduce((subtotal, blog) => subtotal+blog.likes, 0)
+
+module.exports ={ dummy, totalLikes }
